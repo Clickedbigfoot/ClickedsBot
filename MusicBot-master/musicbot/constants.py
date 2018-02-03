c0802c795dc70c04101013300333caadb6a38547
Clicked <clickedbigfoot@gmail.com>
2018-02-03 16:18:22 -0600

    First commit

---
+++ MusicBot-master/musicbot/constants.py
@@ -0,0 +1,10 @@
+import os.path
+
+MAIN_VERSION = '1.9.5_2'
+SUB_VERSION = ''
+VERSION = MAIN_VERSION + SUB_VERSION
+
+AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
+DISCORD_MSG_CHAR_LIMIT = 2000
+PHOTO_CACHE_PATH = os.path.join(os.getcwd(), 'photos')
+AMRIT_CACHE_PATH = os.path.join(os.getcwd(), 'amrit_memes')
